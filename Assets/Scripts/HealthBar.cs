4e55b1ff1b4b6db173f62dbdcc906b773ba89dd2
Vlad818968 <66926263+Vlad818968@users.noreply.github.com>
2022-09-01 17:22:43 +0300

    i

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,43 +7,54 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Text _healthText;
     [SerializeField] private Slider _healthSlider;
+    [SerializeField] Player _player;
 
     private float _health;
     private float _recoveryRate = 25f;
     private Coroutine _healthCoroutine;
-    
+
     private void Start()
     {
+        SetMaxHealth();
+    }
+
+    private void SetMaxHealth()
+    {
+        _healthSlider.maxValue = _player.CurrentHealth;
+        _healthSlider.value = _healthSlider.maxValue;
+        _health = _player.CurrentHealth;
         _healthText.text = _healthSlider.value.ToString();
     }
 
-    public void SetMaxHealth(Player player)
+    private void OnEnable()
     {
-        _healthSlider.maxValue = player.MaxHealth;
-        _healthSlider.value = _healthSlider.maxValue;
-        _health = player.MaxHealth;
+        _player.HealthChanged += StartHealthUpdate;
     }
 
-    public void StartHealthUpdate(Player player)
+    private void OnDisable()
+    {
+        _player.HealthChanged -= StartHealthUpdate;
+    }
+
+    private void StartHealthUpdate(int currentHealth)
     {
         if (_healthCoroutine != null)
         {
             StopCoroutine(_healthCoroutine);
         }
 
-        _healthCoroutine = StartCoroutine(HealthUpdate(player));
+        _healthCoroutine = StartCoroutine(HealthUpdate(currentHealth));
     }
 
-    private IEnumerator HealthUpdate(Player player)
+    private IEnumerator HealthUpdate(int currentHealth)
     {
-        while (_health != player.CurrentHealth)
+        while (_health != currentHealth)
         {
-           _health = Mathf.MoveTowards(_health, player.CurrentHealth, _recoveryRate * Time.deltaTime);
+           _health = Mathf.MoveTowards(_health, currentHealth, _recoveryRate * Time.deltaTime);
             int health = (int)_health;
            _healthText.text = health.ToString();
             _healthSlider.value = health;
             yield return null;
         }
     }
-
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,35 +1,32 @@
 using UnityEngine;
+using UnityEngine.Events;
+using System;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
-    [SerializeField] private HealthBar _healthBar;
-
     private int _currentHealth;
 
     public int CurrentHealth => _currentHealth;
-    public int MaxHealth => _maxHealth;
+
+    public Action<int> HealthChanged;
 
     public void TakeDamage(int damage)
     {
         _currentHealth -= damage;
-        _healthBar.StartHealthUpdate(this);
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        HealthChanged?.Invoke(_currentHealth);
     }
 
     public void RestoreHealth(int treatment)
     {
         _currentHealth += treatment;
-        _healthBar.StartHealthUpdate(this);
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
+        HealthChanged?.Invoke(_currentHealth);
     }
 
     private void Awake()
     {
         _currentHealth = _maxHealth;
-        _healthBar.SetMaxHealth(this);
-    }
-
-    private void Update()
-    {
-        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
     }
 }
